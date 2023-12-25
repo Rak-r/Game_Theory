d9989cfb4379f80bf6717e2915363c43c980ec90
Rak-r <rakshitnar12@gmail.com>
2023-12-25 17:17:40 +0000

    add solution to matching pennies

--- Mixed_Strategy.py
+++ Mixed_Strategy.py
@@ -1,3 +1,31 @@
+'''This script creates a simple game called 'Matching Pennies, in which both players simulataneously reveals a penny/coin.
+If both coins showed heads or both showed tails, Player 1 gets $1 from Player 2. 
+But if one of the pennies showed tails or heads, then Player 2 gets $1
+from Player 1.'''
+
+
+'''There are two action/strategies available--> Heads and Tails
+Payoffs for each players: 
+  
+     __           __
+P1 =|               |
+    |  1       -1   | --> Heads
+    |  -1        1  | --> Tails
+    |__           __| ,
+
+     __ Heads   Tails __
+P2 =|                   |
+    |  -1       1       |
+    |   1       -1      |
+    |__               __|    
+
+    
+I have calculated the mixed srategy nash equilibrium manually to verify the concept and to understand the working of Gambit API and methods
+ 
+'''
+
+
+
 import pygambit 
 from pygambit import gambit
 import numpy as np
@@ -22,24 +50,25 @@ class MatchingPennies():
         self.solve_game()
 
     def solve_game(self):
-        '''As you can observe, the payoofs provided above represents a zero-sum game meaning cancelling each other, 
+        '''As you can observe, the payoffs provided above represents a zero-sum game meaning cancelling each other, 
         so there will be no pure nash equilibirum exists in such cases'''
 
         pure_nash_equilibrium = pygambit.nash.enumpure_solve(self.create_game)
         print('Pure Nash equilibrium', pure_nash_equilibrium)
 
-        '''However, there must exist a mixed startegy nash equilibrium which can be computed using enummixed method'''
+        '''However, there must exist a mixed strategy nash equilibrium which can be computed using enummixed method'''
         mixed_staregy_nash = pygambit.nash.enummixed_solve(self.create_game)
         print('Mixed startegy nash', mixed_staregy_nash)
         
         if mixed_staregy_nash:
-            '''want to look for the expected utilities/payoffs for  the players'''
-            expected_payoffs = self.create_game.mixed_strategy_profile()
+            
+            expected_payoffs = self.create_game.mixed_strategy_profile()                    # want to look for the expected utilities/payoffs for  the players 
             print('expected_payoffs:', expected_payoffs)
 
 
 def main():
     matching_pennies = MatchingPennies('Rakshit', 'Charles')
     print('game is created', matching_pennies.create_game)
+
 if __name__ == '__main__':
     main()
